8734a2ab17d9ab97c7c77afdb82dde0277a7a4b2
João Luis Hilario Teixeira <93398469+JackSouls@users.noreply.github.com>
2026-08-06 11:33:33 -0300

    Add functions for printing and summing integers

---
+++ funcoes/funcoes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "funcoes.h"
+
+int fazAlgo(int a){
+    printf("\nValor : %d", a);
+    a = 100;
+    printf("\nValor : %d", a);
+    return a;
+}
+
+int soma(int num1, int num2){
+    
+    return num1 + num2;
+}

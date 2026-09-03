eb0eb0e2cf022c196c40f98e5dc2e54d3be107af
João Luis Hilario Teixeira <93398469+JackSouls@users.noreply.github.com>
2026-09-03 10:51:20 -0300

    Fix struct definition and variable initialization

--- beecrowd/2692.c
+++ beecrowd/2692.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef sctruct{
+typedef struct{
     char texto[50];
 } Bagunca;
  
 int main() {
-    int n;
-    int m;
-
-    Bagunça bagunca[m];
-    
+    int n = 0;
+    int m = 0;
 
     scanf("%d", &n);
     scanf("%d", &m);
 
+    Bagunca bagunca[m];
 
-    int nm [n][2];
+    char nm[n][2];
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < 2; j++){
@@ -26,16 +24,22 @@ int main() {
 
    for(int i = 0; i < m; i++){
     fgets(bagunca[i].texto, 50, stdin);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
    }
 
    for(int i = 0; i < m; i++){
         for(int j = 0; j < strlen(bagunca[i].texto); j++){
             for(int k = 0; k < n; k++){
-                
+                if(bagunca[i].texto[j] == nm[k][0]){
+                    bagunca[i].texto[j] = nm[k][1];
+                } else if(bagunca[i].texto[j] == nm[k][1]) {
+                    bagunca[i].texto[j] = nm[k][0];
+                }
             }
         }
    }
-
- 
+    return 0;
+}
     return 0;
 }

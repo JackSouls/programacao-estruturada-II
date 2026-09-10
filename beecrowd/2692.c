9c233e2ac39c9b0808e29927c5d4d6af975fe24d
João Luis Hilario Teixeira <93398469+JackSouls@users.noreply.github.com>
2026-09-10 10:57:09 -0300

    Refactor input handling and remove unused struct

--- beecrowd/2692.c
+++ beecrowd/2692.c
@@ -1,43 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct{
-    char texto[50];
-} Bagunca;
  
 int main() {
-    int n = 0;
-    int m = 0;
+    int numLetras = 0;
+    int numFrases = 0;
 
-    scanf("%d", &n);
-    scanf("%d", &m);
+    scanf(" %d %d", &numFrases, &numLetras);
 
-    Bagunca bagunca[m];
+    char trocas[numLetras][2];
+    char frase[numFrases][100];
 
-    char nm[n][2];
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < 2; j++){
-            scanf("%c", &nm[i][j]);
-        }
+    for (int i = 0; i < numLetras; i++)
+    {
+        scanf(" %c %c", &trocas[i][0], trocas[i][1]);
     }
 
-   for(int i = 0; i < m; i++){
-    fgets(bagunca[i].texto, 50, stdin);
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-   }
+    for (int i = 0; i < numFrases; i++)
+    {
+        scanf(" %[^\n]", &frase[i]);
+    }
 
-   for(int i = 0; i < m; i++){
-        for(int j = 0; j < strlen(bagunca[i].texto); j++){
-            for(int k = 0; k < n; k++){
-                if(bagunca[i].texto[j] == nm[k][0]){
-                    bagunca[i].texto[j] = nm[k][1];
-                } else if(bagunca[i].texto[j] == nm[k][1]) {
-                    bagunca[i].texto[j] = nm[k][0];
-                }
-            }
-        }
-   }
-    return 0;
+
+    
+    
+    
+    
+
 }

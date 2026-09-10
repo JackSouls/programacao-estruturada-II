2b2fe14fb040fb80d0c9054114df56ad1e9c8b25
João Luis Hilario Teixeira <93398469+JackSouls@users.noreply.github.com>
2026-09-10 12:55:16 -0300

    Fix input handling and enhance variable naming
    Rearranged scanf order and updated variable names for clarity. Added missing loops for reading and printing phrases.

--- beecrowd/2692.c
+++ beecrowd/2692.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
- 
-int main() {
+int main(){
     int numLetras = 0;
     int numFrases = 0;
 
-    scanf(" %d %d", &numFrases, &numLetras);
+    scanf(" %d %d", &numLetras, &numFrases);
 
     char trocas[numLetras][2];
-    char frase[numFrases][100];
+    char frases[numFrases][100];
 
     for (int i = 0; i < numLetras; i++)
     {
-        scanf(" %c %c", &trocas[i][0], trocas[i][1]);
+        scanf(" %c %c", &trocas[i][0], &trocas[i][1]);
+    }
+    
+    for (int i = 0; i < numFrases; i++)
+    {
+        scanf(" %[^\n]", frases[i]);
     }
 
     for (int i = 0; i < numFrases; i++)
     {
-        scanf(" %[^\n]", &frase[i]);
+        for (int j = 0; j < numLetras; j++)
+        {
+            for (int k = 0; k < strlen(frases[i]); k++)
+            {
+                if(frases[i][k] == trocas[j][0]){
+                    frases[i][k] = trocas[j][1];
+                } else if(frases[i][k] == trocas[j][1]){
+                    frases[i][k] = trocas[j][0];
+                }
+            }
+        }
     }
 
-
-    
-    
-    
-    
-
+    for (int i = 0; i < numFrases; i++)
+    {
+        printf("%s\n", frases[i]);
+    }
+    return 0;
 }
